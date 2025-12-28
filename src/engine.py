a6b4eb4a278f95b5c401aa565d9ad1b5edb71136
Marko Petrov <petrovmarko2006@gmail.com>
2025-12-28 21:59:09 +0100

    Update engine.py

--- src/engine.py
+++ src/engine.py
@@ -1,27 +1,38 @@
 # Neural Network Engine (MLP)
-
+# currently it does not support paralel computation (whole mini_batch at once)
 from scalar import Scalar
 import random
 
 class Parameter:
-    def __init__(self, in_features, scale, activation = True):
-        self.w = [Scalar(random.random() * scale) for x in range(in_features)]
-        self.b = Scalar(random.random() * scale)
+    def __init__(self, in_features, activation):
+        '''contains w_i for each in feature and one bias
+        the activation is a seperate class'''
+        self.w = [Scalar(random.random()) for x in range(in_features)]
+        self.b = Scalar(random.random())
         self.in_features = in_features
-        self.activation = activation
+        if activation == 'relu':
+            self.activation = Scalar.ReLU
+        else:
+            self.activation = lambda x : x
     
     def forward(self, X):
+        # Make sure it's in the form of array
         if isinstance(X, (float, int, Scalar)):
             X = [X]
-        return sum([X[i] * self.w[i] for i in range(self.in_features)]) + self.b
+        return self.activation(sum([X[i] * self.w[i] for i in range(self.in_features)]) + self.b)
     
     def __call__(self, X):
         return self.forward(X)
     
+    def __mul__(self, y):
+        self.w = [w * y for w in self.w ]
+        self.b = self.b * y
+        return self
+    
 class Layer:
 
     def __init__(self, in_feature, out_features, scale = 10, activation = True):
-        self.layer = [Parameter(in_feature, activation, scale) for i in range(out_features)]
+        self.layer = [Parameter(in_feature, activation) * scale for i in range(out_features)]
 
     def forward(self, X):
         return [parameter(X) for parameter in self.layer]
@@ -31,25 +42,32 @@ class Layer:
 
 class NeuralNet:
     def __init__(self, lr):
+        '''only learning rate as parameter'''
         self.layers = []
         self.parameters = []
         self.lr = lr
         return
     
     def add_layer(self, layer):
+        '''Add a layer to the network'''
         self.layers.append(layer)
         for neuron in layer.layer:
             self.parameters.append(neuron)
     
     def forward(self, X):
+        '''process the layers one by one and substituting X as the result'''
+        # X -> layer1(x) -> layer2(layer1(x)) -> ...
         for layer in self.layers:
             X = layer(X)
+
+        # If X is length one, just return the only element
         return X if len(X) > 1 else X[0]
     
     def __call__(self, X):
         return self.forward(X)
     
     def zero_grad(self):
+        '''Zero out the gradients of all parameters'''
         for param in self.parameters:
             for neuron in param.w:
                 neuron.grad = 0
@@ -59,18 +77,10 @@ class NeuralNet:
             
 
     def step(self):
+        '''Update the values of the parameters'''
         for param in self.parameters:
             for neuron in param.w:
-                if neuron.grad > 10:
-                    neuron.grad = 10
-                if neuron.grad < -10:
-                    neuron.grad = -10
+                neuron.clip_grad(-10,10) # clip gradients to not explode
                 neuron.value -= self.lr * neuron.grad
-            if param.b.grad > 10:
-                param.b.grad = 10
-            if param.b.grad < -10:
-                param.b.grad = -10
+            param.b.clip_grad(-10,10) # clip gradients to not explode
             param.b.value -= self.lr * param.b.grad
-
-
-    
